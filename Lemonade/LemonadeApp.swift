e436f0625fa83e8e63a1491d690f526ed75ff250
J-Mowbray <j.mowbray96@gmail.com>
2024-10-12 16:04:41 +0100

    Initial Commit

---
+++ Lemonade/LemonadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonadeApp.swift
+//  Lemonade
+//
+//  Created by Jamie Mowbray on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LemonadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LemonadeTests/LemonadeTests.swift
@@ -0,0 +1,17 @@
+//
+//  LemonadeTests.swift
+//  LemonadeTests
+//
+//  Created by Jamie Mowbray on 12/10/2024.
+//
+
+import Testing
+@testable import Lemonade
+
+struct LemonadeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
